8b38ac3a692fbf61502c6a9f3c3eb209ec62fea1
ATone <j033312003@gmail.com>
2017-11-05 20:53:22 +0800

    增加AutoClicker

---
+++ coockieClicker/AutoClick.js
@@ -0,0 +1,30 @@
+var autoClick = {
+	freq = 20000,
+	GoldenCookie,
+	Raeindeer,
+	state: function (flag) {
+		if (flag) {
+			autoClick.GoldenCookie = setInterval(function () {
+					for (var h in Game.shimmers) {
+						if (Game.shimmers[h].type == "golden") {
+							Game.shimmers[h].pop();
+						}
+					}
+				}, autoClick.freq);
+			autoClick.Raeindeer = setInterval(function () {
+					for (var h in Game.shimmers) {
+						if (Game.shimmers[h].type == "reindeer") {
+							Game.shimmers[h].pop();
+						}
+					}
+				}, autoClick.freq);
+		} else {
+			clearInterval(autoClick.GoldenCookie);
+			clearInterval(autoClick.Raeindeer);
+		}
+	}
+};
+
+autoClick.state(true);
+
+
