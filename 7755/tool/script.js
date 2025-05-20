2c15f0f0ae0cd2bd98bee8adbfd8b8089bb0a77b
TingChunKuo <j033312003@gmail.com>
2025-05-20 20:26:04 +0800

    增加markdown縮排小工具

--- 7755/tool/script.js
+++ 7755/tool/script.js
@@ -5,6 +5,22 @@ function cal(){
   
 }
 
+function indent(){
+	var content = ' - '+$('#mk').val().replaceAll('\n', '\n - ');
+	$('#mk').val(content);
+}
+
+function addNo(){
+	var lines = $('#mk').val().split('\n');
+	var content = '';
+	for(var i=0;i<lines.length;i++){
+		var j = i+1;
+		content += j+'. '+lines[i]+'\n';
+	}
+	
+	$('#mk').val(content);
+}
+
 $(document).on('change', '#ct', function(){
 	
 	cal();
